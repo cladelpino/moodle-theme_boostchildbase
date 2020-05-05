a412627dc945e07547e3359f2e1ff073f4f35c72
cladelpino <cladelpino@gmail.com>
2020-05-05 16:50:10 -0300

    Update version.php

--- version.php
+++ version.php
@@ -15,25 +15,24 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Photo backgrounds.
+ * Look and feel pvirtual
  *
- * @package    theme_photos
- * @copyright  2016 Damyon Wiese
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    tema_pvirtual
+  *@license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2017022000;
+$plugin->version = 2020050500;
 
-// This is the version of Moodle this plugin requires.
-$plugin->requires = 2016112900.00;
+// This is the -minimal- version of Moodle this plugin requires.
+$plugin->requires = 2016112900;
 
 // This is the component name of the plugin - it always starts with 'theme_'
 // for themes and should be the same as the name of the folder.
-$plugin->component = 'theme_photo';
+$plugin->component = 'theme_pvirt';
 
 // This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
@@ -44,5 +43,5 @@ $plugin->dependencies = [
 $plugin->maturity = MATURITY_STABLE;
 
 // This is the named version.
-$plugin->release = 1.3;
+$plugin->release = 0.1;
 
